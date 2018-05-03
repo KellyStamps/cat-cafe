60b17779b429e6a9c2201e0ab3bbc6f0ce3154db
KellyStamps <kellydsample@gmail.com>
2018-05-03 11:21:43 -0400

    added very simple outline

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello world!</h1>
+        <div>Do I need the navbar?</div>
+        <h1>Welcome to the cat cafe</h1>
+        <div>I'm a welcome paragraph with a cute carousel of pictures</div>
+        <div>I'm the menu</div>
+        <div>I'm the merch</div>
+        <div>I'm the contact information</div>
+        <div>I'm the footer</div>
       </div>
     );
   }
